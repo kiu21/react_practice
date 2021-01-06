5460cfde36a79a5bb3431e3f85f40c0abdea6bbf
kiu21 <18024603+kiu21@users.noreply.github.com>
2021-01-06 20:17:40 +0900

    iiii

---
+++ github.js
@@ -0,0 +1,12 @@
+for(var i=0;i<memoListItem.length;i++){
+    var item=memoListItem[i];
+    item.style.backgroundColor='#eeeeee';}
+
+
+    var a = text=='test'?1:2
+
+
+//githubに秘密鍵を登録
+ ssh -T git@github.com -i ~/.ssh/kiu_github
+//githubに接続しているかの確認
+ssh -T git@github.com  
